cb28eb60e9ca85c1be2b967753575aa7a6018cdf
ilia.brykin <brykin.ilia@gmail.com>
2025-02-16 17:34:17 +0100

    Init plugin and 2 rules

---
+++ index.js
@@ -0,0 +1,13 @@
+import groupImports from "./lib/rules/group-imports";
+import namedImports from "./lib/rules/named-imports";
+
+export default {
+  meta: {
+    name: "eslint-plugin-group-imports",
+    version: "1.0.0",
+  },
+  rules: {
+    "group-imports": groupImports,
+    "named-imports": namedImports,
+  },
+};
