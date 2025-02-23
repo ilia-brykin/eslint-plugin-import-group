4057ce357be2f9336cf958ec65b46abcee41389d
ilia.brykin <brykin.ilia@gmail.com>
2025-02-23 14:52:20 +0100

    Script for all test

---
+++ tests/rules/group-imports/index.js
@@ -0,0 +1,6 @@
+import "./basic/test-basic.js";
+import "./emptyLinesBeforeGroup/test-emptyLinesBeforeGroup.js";
+import "./emptyLinesBetweenGroups/test-emptyLinesBetweenGroups.js";
+import "./includes/test-includes.js";
+import "./preserveNonImportCode/test-preserveNonImportCode.js";
+import "./sort/test-sort.js";
---
+++ tests/rules/index.js
@@ -0,0 +1,1 @@
+import "./group-imports/index.js";
